a83ba0de0c34cb306ad02ad491609244a0631526
Sanjish Chouhan <chouhansanjish@gmail.com>
2025-04-22 19:15:05 +0000

    convertion_operation

---
+++ JavaScript_Basic_01/convertion_Operation.js
@@ -0,0 +1,90 @@
+// let number="33"
+ 
+// console.log(typeof number);
+
+// let convert_Number= Number(number)
+// console.log(convert_Number);
+// console.log(typeof convert_Number);
+
+// let undefined2 ="33abcd"
+// console.log(typeof undefined2);
+
+// let convert_undefined = Number(undefined2);
+// console.log(convert_undefined);
+// console.log(typeof convert_undefined);
+
+// note string 33 = 33 ho jata h 
+
+
+// let number= null
+ 
+// console.log(typeof number);
+
+// let convert_Number= Number(number)
+// console.log(convert_Number);
+// console.log(typeof convert_Number);
+
+// let undefined2 ="33abcd"
+// console.log(typeof undefined2);
+
+// let convert_undefined = Number(undefined2);
+// console.log(convert_undefined);
+// console.log(typeof convert_undefined);
+ 
+// note null value number deti h 0 or string 33abc Nan deti h or uska datatype Number deta h 
+
+// let number= undefined 
+ 
+// console.log(typeof number);
+
+// let convert_Number= Number(number)
+// console.log(convert_Number);
+// console.log(typeof convert_Number);
+
+// let undefined2 ="33abcd"
+// console.log(typeof undefined2);
+
+// let convert_undefined = Number(undefined2);
+// console.log(convert_undefined);
+// console.log(typeof convert_undefined);
+
+
+
+
+// let number= true 
+ 
+// console.log(typeof number);
+
+// let convert_Number= Number(number)
+// console.log(convert_Number);
+// console.log(typeof convert_Number);
+
+// let undefined2 ="33abcd"
+// console.log(typeof undefined2);
+
+// let convert_undefined = Number(undefined2);
+// console.log(convert_undefined);
+// console.log(typeof convert_undefined);
+
+// note ture value convert ke b ad 1 hoti h to false 0 hoga 
+
+let isboolean1 = 1;
+let isboolean2 = "";
+let isboolean3 = "sanjish";
+let isboolean4 = undefined ;
+let isboolean5 = null ;
+
+let IsBooleanValue=Boolean(isboolean1)
+console.log(IsBooleanValue);
+
+let IsBooleanValue2=Boolean(isboolean2)
+console.log(IsBooleanValue2);
+
+let IsBooleanValue3=Boolean(isboolean3)
+console.log(IsBooleanValue3);
+
+let IsBooleanValue4=Boolean(isboolean4)
+console.log(IsBooleanValue4);
+
+let IsBooleanValue5=Boolean(isboolean5)
+console.log(IsBooleanValue5);
