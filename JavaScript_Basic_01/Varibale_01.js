5e75afa8e46134f96b310d8a143a170b40f51d1c
Sanjish Chouhan <chouhansanjish@gmail.com>
2025-04-22 18:43:00 +0000

    Variable_DataType

---
+++ JavaScript_Basic_01/Varibale_01.js
@@ -0,0 +1,21 @@
+const accountId =1411
+let accountName ="SANJISH CHOUHAN "
+var accountEmail ="abc@gmail.com"
+accountCity="Chhatisgarh"
+// console.log(accountId);
+// console.log(accountName);
+// console.log(accountEmail);
+// console.log(accountCity);
+// accountId=521
+// console.log(accountId);
+accountEmail="122@gmail.com"
+accountName="sanjay chouhan"
+accountCity="anda"
+let accountState;
+// console.log(accountId);
+// console.log(accountName);
+// console.log(accountEmail);
+// console.log(accountCity);
+console.table([accountId,accountName,accountEmail,accountCity,accountState])
+
+
