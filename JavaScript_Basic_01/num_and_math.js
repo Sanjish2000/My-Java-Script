f7775938188d9394a3804d6cc84893592134ac92
Sanjish Chouhan <chouhansanjish@gmail.com>
2025-04-23 19:50:06 +0000

    num  and maths

---
+++ JavaScript_Basic_01/num_and_math.js
@@ -0,0 +1,53 @@
+const age = 25
+console.log(age);
+
+const balance=new Number(5000)
+console.log(balance);
+console.log(balance.toFixed(3));
+
+
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+console.log(balance.toString().length);
+
+const number = 24.685
+console.log(number.toPrecision(2));
+
+console.log(number.toExponential(3));
+const cash=1000000000
+console.log(cash.toLocaleString('en-IN'));
+
+
+
+// **************math*********
+
+let num2=-100;
+let a= 3.1
+let b=3.9
+console.log(Math.abs(num2));
+console.log(Math.round(a));
+console.log(Math.ceil(a));
+console.log(Math.floor(b));
+
+
+let c=12 
+let k=334
+let l=3435
+// console.log(typeof(abc));
+// console.log(abc);
+console.log(Math.max(c,k,l));
+console.log(Math.min(c,k,l));
+
+
+let min=1
+let max =6
+
+console.log(Math.floor(Math.random()*(max-min+1)+min)); 
+
+
+
+
+
+
+
+
