409ade51bc95e2777c7e537781c21804f94b4e80
Sanjish Chouhan <chouhansanjish@gmail.com>
2025-04-23 19:02:40 +0000

    string

--- JavaScript_Basic_01/data_type_summary.js
+++ JavaScript_Basic_01/data_type_summary.js
@@ -35,3 +35,28 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// ***********STACK(primitive data types) AND HEAP (non primitive)********
+
+let name="SANJISH CHOUHAN"
+
+let name2=name;
+
+name2="sanjay chouhan"      
+console.log(name);
+console.log(name2);
+
+// heap 
+
+let myprofile ={
+    name:"sanjish",
+    age:15
+
+}
+let upgradeprofile=myprofile
+
+upgradeprofile.age=55;
+
+console.log(myprofile);
+console.log(upgradeprofile);
