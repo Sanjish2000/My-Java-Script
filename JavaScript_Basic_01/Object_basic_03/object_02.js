8fc6b12d68b476b0f4e1f1123e8c7dc93e9f6dba
Sanjish Chouhan <chouhansanjish@gmail.com>
2025-04-26 20:24:24 +0000

    object3

---
+++ JavaScript_Basic_01/Object_basic_03/object_02.js
@@ -0,0 +1,79 @@
+const sanjish2=new Object()  //singletone object
+
+const sanjish = {    //non single tone object
+
+}
+// console.log(sanjish2);
+// console.log(sanjish);
+
+const myDetails={
+
+    email:"abc2gmail.com",
+    fullname:{
+        username:{
+            firstName:"sanjish",
+            lastName:"chouhan",
+        }
+    }
+
+}
+// console.log(myDetails);
+// console.log(myDetails.fullname);
+// console.log(myDetails.fullname.username);
+
+const obj1={1:"a" , 2:"b"}
+const obj2={3:"c" , 4:"d"}
+ 
+// const obj3 = Object.assign({},obj1,obj2)
+// console.log(obj3);
+
+const obj3 ={
+    ...obj1,...obj2
+}
+console.log(obj3);
+
+const aarya_Object=[
+    {
+        name:"abc",
+        email:"abc@gmail.com"
+    },
+    {
+        name:"abc",
+        email:"abc@gmail.com"
+    },
+    {
+        name:"abc",
+        email:"abc@gmail.com"
+    },
+    {
+        name:"abc",
+        email:"abc@gmail.com"
+    }
+]
+// console.log(aarya_Object[1].email);
+
+// console.log(Object.keys(myDetails));
+
+// console.log(Object.entries(myDetails));
+// console.log(myDetails.hasOwnProperty("fullname"));
+
+
+const date ={
+    day:1,
+    month:"jan",
+    year:"2025"
+}
+
+const {day:d,month:m, year:y}=date   // object destractor
+console.log(d);
+console.log(m)
+console.log(y);
+;
+
+
+
+
+
+
+
+
